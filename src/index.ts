fb164d924590da2091a454b0cff9d28bb969e264
Chandan Chauhan <bellicose100xp@users.noreply.github.com>
2023-10-17 09:59:51 -0700

    sign in and sign up routes added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+
+import { connect } from "./db/mongodb.js";
+
+/* API Routers */
+import authRouter from "./routes/authRouter.js";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+/* Use Routers */
+app.use("/auth", authRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
+    connect();
 });
-
-
-app.listen(3000, () => {
-    console.log("Listening..")
-})
---
+++ src/routes/authRouter.ts
@@ -0,0 +1,58 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import { User } from "../models/users.js";
+
+const router = express.Router();
+
+router.post("/signup", async (req, res) => {
+    const { email, password, url } = req.body;
+
+    if (!email || !password || !url) {
+        return res.status(400).send({ status: "Email, Password, and URL Required" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        return res.status(400).send({ error: `User with email ${email} already exists` });
+    }
+
+    const existingURL = await User.findOne({ url });
+    if (existingURL) {
+        return res
+            .status(400)
+            .send({ error: `URL ${url} is already taken, please choose a different URL` });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ email, password: hashedPassword, url });
+
+    res.send(newUser);
+});
+
+router.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).send({ error: "Email or password missing!" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return res.status(500).send({ error: "User not found!" });
+    }
+
+    if (!user.password) {
+        return res.status(500).send({ error: "An unexpected error has happened!" });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+        return res.status(400).send({ error: "Invalid password" });
+    }
+
+    res.send(user);
+});
+
+export default router;
